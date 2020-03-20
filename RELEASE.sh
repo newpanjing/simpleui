3ce8e3c93a84935c099a0b7e9f7b0c0f77d6ef8f
panjing <599194993@qq.com>
2020-03-20 14:01:23 +0800

    release 2.9.2

--- RELEASE.sh
+++ RELEASE.sh
@@ -1,4 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
+#echo 'release django-simpleui...'
+#rm -rf dist/*
+#python3 setup.py sdist
+#twine upload dist/*
+#
+#echo 'release django-simpleui success.'
+
+echo 'release simpleui...'
 rm -rf dist/*
-python3 setup.py sdist
+python3 setup2.py sdist
 twine upload dist/*
+
+echo 'release simpleui success.'
