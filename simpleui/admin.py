16fb109bd05d345f7e61921da6cf13f3d9a4c3b8
newpanjing <599194993@qq.com>
2022-03-13 18:42:32 +0800

    fix bug

--- simpleui/admin.py
+++ simpleui/admin.py
@@ -1,9 +1,17 @@
+import json
+
 from django.contrib import admin
 from django.urls import path
 
 
 class AjaxAdmin(admin.ModelAdmin):
+    """
+    This class is used to add ajax functionality to the admin interface.
+    """
     def callback(self, request):
+        """
+        This method is used to handle ajax requests.
+        """
         post = request.POST
         action = post.get("_action")
         selected = post.get("_selected")
@@ -19,10 +27,35 @@ class AjaxAdmin(admin.ModelAdmin):
             if select_across == "0":
                 if selected and selected.split(","):
                     queryset = queryset.filter(pk__in=selected.split(","))
+            else:
+                # 过滤搜索条件，自simpleui-2022.3.13版本开始，支持搜索条件过滤，simplepro需要3.4.2及以上版本
+                # 只有选择全部的时候才过滤数据
+                # 字段为_search和_filter 是为了防止命名冲突
+
+                # search
+                if hasattr(self, "search_fields") and "_search" in post:
+                    search_fields = self.get_search_fields(request)
+
+                    if search_fields:
+                        search_value = post.get("_search")
+                        if search_value:
+                            queryset = queryset.filter(**{
+                                "{}__icontains".format(field): search_value
+                                for field in search_fields
+                            })
+                # filter条件过滤
+                if "_filter" in post:
+                    filter = post.get("_filter")
+                    if filter:
+                        filter_value = json.loads(filter)
+                        queryset = queryset.filter(**filter_value)
 
             return func(self, request, queryset)
 
     def get_urls(self):
+        """
+        This method is used to add ajax functionality to the admin interface.
+        """
         info = self.model._meta.app_label, self.model._meta.model_name
 
         return super().get_urls() + [
